ff17d0d0fc32e209f8467d5452eecded3187d88a
JoseAngara <58715908+JoseAngara@users.noreply.github.com>
2022-03-25 18:10:14 -0600

    Add files via upload

---
+++ dist/pricingRange.js
@@ -0,0 +1,16 @@
+let prices = [
+    { pageviews: "10K", price: 8 },
+    { pageviews: "50K", price: 12 },
+    { pageviews: "100K", price: 16 },
+    { pageviews: "500K", price: 24 },
+    { pageviews: "1M", price: 36 }
+];
+let pageviews = document.getElementById("pageviews");
+let price = document.getElementById("price");
+let planSlider = document.getElementById("planSlider");
+let billing = document.getElementById("billing");
+function loadData() {
+    let planInfo = prices[planSlider.value];
+    pageviews.innerText = `${planInfo.pageviews} PAGEVIEWS`;
+    price.innerText = billing.checked ? `$${planInfo.price * 0.75}.00` : `$${planInfo.price}.00`;
+}
